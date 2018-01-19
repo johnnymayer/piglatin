1b40d1546f1440ebcc8c1884438e6467cd77379c
Frank Ngo and Johnny Mayer <frank@>
2018-01-18 16:12:19 -0800

    Add more regexp to functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,32 @@
-function pigLatin(phrase){
-  if(!isNaN(phrase.charAt(0))){
-    return false;
-  }
-  else if(phrase.charAt(0) === "a" || phrase.charAt(0) === "e" || phrase.charAt(0) === "i" || phrase.charAt(0) === "o" || phrase.charAt(0) === "u"){
-    phrase = phrase + "way";
-    return phrase;
-  } else if (phrase.charAt(0) != "a" || phrase.charAt(0) != "e"|| phrase.charAt(0) != "i"|| phrase.charAt(0) != "o" || phrase.charAt(0) != "u"){
-    phrase = phrase.replace(/(\w+?)([aeiou]\w+)/i, '$2$1') + "ay";
-    return phrase;
-  } else if (phrase[0] === "q" && phrase[1] === "u"){
-      phrase = phrase.charAt(0,1) + "quay";
+function pigLatin(phrase) {
+  if (phrase.match(/[a-z]/)) {
+    if (phrase.charAt(0) === "q" && phrase.charAt(1) === "u") {
+      phrase = phrase.slice(2) + "quay";
       return phrase;
-  }
-};
+    } else if (phrase.charAt(0) === "a" || phrase.charAt(0) === "e" || phrase.charAt(0) === "i" || phrase.charAt(0) === "o" || phrase.charAt(0) === "u") {
+      return phrase.replace(/^([aeiouy])(.*)/, '$1$2way');
+    } else if (phrase.charAt(0) != "a" || phrase.charAt(0) != "e" || phrase.charAt(0) != "i" || phrase.charAt(0) != "o" || phrase.charAt(0) != "u") {
+      return phrase.replace(/^([^aeiouy]+)(.*)/, '$2$1ay');
+    } else {
+      return false;
+    }
+    }
+  };
 
 $(document).ready(function() {
   $("form#translated-form").submit(function(event){
     event.preventDefault();
     var newArray = [];
-    var phrase = $("input#phrase").val().split(/\b\W\S/i);
+    var phrase = $("input#phrase").val().toLowerCase().split(/\b/i);
     for (var i = 0; i < phrase.length; i ++) {
       newArray.push(pigLatin(phrase[i]));
     }
 
-    $(".phrase").text(phrase);
+    $(".phrase").text(newArray.join(" "));
 
-    if(!result){
-      $(".pig-latin").text("This can not be translated to pig latin");
-    } else {
-      $(".pig-latin").text(newArray);
-    }
+    // if(!result){
+    //   $(".pig-latin").text("This can not be translated to pig latin");
+    // } else {
+    //   $(".pig-latin").text(newArray);
   });
 });
